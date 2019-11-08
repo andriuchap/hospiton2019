a36f4f86bd7d23cc7ce260e173bd74fc368a20ab
Andrius <andr.paulauskas@gmail.com>
2019-11-08 21:31:19 +0200

    initial project commit

---
+++ Source/Hospiton19/Hospiton19GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Hospiton19GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOSPITON19_API AHospiton19GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
