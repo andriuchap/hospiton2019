becf0e4a5c18bdb2c68974ae98d838f3a6cace98
vilrin <51914948+vilrin@users.noreply.github.com>
2019-11-10 11:39:13 +0200

    pjio

---
+++ Source/Hospiton19/Interactables/Door.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Door.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ArrowComponent.h"
+
+// Sets default values
+ADoor::ADoor(const FObjectInitializer &ObjInitializer)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	RootComponent = RootScene;
+
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+	DoorMesh->SetupAttachment(RootScene);
+	DoorMesh->SetCollisionProfileName(FName("BlockAllDynamic"));
+
+	HandlePoint = CreateDefaultSubobject<UArrowComponent>(TEXT("HandlePoint"));
+	HandlePoint->SetupAttachment(DoorMesh);
+	HandlePoint->SetHiddenInGame(true);
+}
+
+// Called when the game starts or when spawned
+void ADoor::BeginPlay()
+{
+	Super::BeginPlay();
+	ClosedRotation = DoorMesh->GetComponentRotation();
+}
+
+// Called every frame
+void ADoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (PullingComponent != nullptr)
+	{
+		FVector DoorHandleLocation = HandlePoint->GetComponentLocation();
+		FVector PullingComponentLocation = PullingComponent->GetComponentLocation();
+		// Assumes that the root location of the door mesh is at the hinge!
+		FVector DoorHingeLocation = DoorMesh->GetComponentLocation();
+		// put all positions on the same plane.
+		DoorHandleLocation.Z = PullingComponentLocation.Z = DoorHingeLocation.Z = 0;
+
+		FVector HingeToPuller = PullingComponentLocation - (DoorHingeLocation + DoorHandleOffset);
+		FRotator NewDoorRotation = HingeToPuller.ToOrientationRotator();
+
+		DoorMesh->SetWorldRotation(NewDoorRotation);
+	}
+}
+
+void ADoor::StartPulling_Implementation(USceneComponent * InPullingComp)
+{
+	PullingComponent = InPullingComp;
+	DoorHandleOffset = InPullingComp->GetComponentLocation() - HandlePoint->GetComponentLocation();
+	DoorHandleOffset.Z = 0;
+}
+
+void ADoor::StopPulling_Implementation()
+{
+	PullingComponent = nullptr;
+}
+
---
+++ Source/Hospiton19/Interaction/PullableActor.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PullableActor.h"
+
+// Add default functionality here for any IPullableActor functions that are not pure virtual.
